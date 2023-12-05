f43c6b477168224289322a3d9e2afbd145a000da
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2023-12-05 09:43:21 +0000

    day 5

---
+++ 05-12-2/lib/challenge/challenge.go
@@ -0,0 +1,196 @@
+package challenge
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type SeedsMap struct {
+	Start int
+	Len   int
+}
+
+type ValMap struct {
+	Rules []ValStruct
+}
+
+func (v *ValMap) Calc(i int) int {
+	for _, r := range v.Rules {
+		prod := r.Calc(i)
+		if prod != i {
+			return prod
+		}
+	}
+	return i
+}
+
+type ValStruct struct {
+	DstStart int
+	SrcStart int
+	RangeLen int
+}
+
+func (v *ValStruct) Calc(i int) int {
+	srcEnd := v.SrcStart + v.RangeLen
+	if i < v.SrcStart {
+		return i
+	} else if i < srcEnd {
+		return (i - v.SrcStart) + v.DstStart
+	} else {
+		return i
+	}
+}
+
+func Challenge(input string) string {
+
+	overallMap := map[string]*ValMap{}
+
+	lines := strings.Split(input, "\n")
+
+	currentKey := ""
+	seeds := []SeedsMap{}
+
+	mapList := []string{}
+
+	for _, l := range lines {
+		// fmt.Printf("Line %d\n", i+1)
+		if l == "" {
+			continue
+		}
+		if strings.Contains(l, ":") {
+			// fmt.Println("Key Line")
+			if strings.HasPrefix(l, "seeds:") {
+				parts := strings.Split(l, ":")
+				nums := strings.Split(parts[1], " ")
+
+				iNums := []int{}
+
+				for _, n := range nums {
+					if n == "" {
+						continue
+					}
+
+					vInt, err := strconv.Atoi(n)
+					if err != nil {
+						panic("int conv error")
+					}
+					iNums = append(iNums, vInt)
+				}
+
+				for i := 0; i < len(iNums); i += 2 {
+					start, rLen := iNums[i], iNums[i+1]
+					seeds = append(seeds, SeedsMap{start, rLen})
+				}
+			} else {
+				parts := strings.Split(l, ":")
+				words := strings.Split(parts[0], " ")
+				if words[1] != "map" {
+					panic("expected map")
+				}
+				names := strings.Split(words[0], "-")
+				if names[1] != "to" {
+					panic("expected to")
+				}
+				currentKey = fmt.Sprintf("%s-%s", names[0], names[2])
+				mapList = append(mapList, currentKey)
+			}
+		} else {
+			// fmt.Println("Number Line")
+			nums := strings.Split(l, " ")
+			// fmt.Println(nums)
+			n := []int{}
+			for _, num := range nums {
+				if num == "" {
+					continue
+				}
+				vInt, err := strconv.Atoi(num)
+				if err != nil {
+					panic("int conv error")
+				}
+				n = append(n, vInt)
+			}
+
+			if len(n) != 3 {
+				panic("incorrect len number list")
+			}
+
+			existing := overallMap[currentKey]
+			overallMap[currentKey] = produceRange(n[0], n[1], n[2], existing)
+		}
+	}
+
+	path := findPath(mapList, "seed-location")
+
+	minLoc := math.MaxInt
+
+	for i, seed := range seeds {
+		fmt.Printf("Seed Range %d\n", i+1)
+		for i := 0; i < seed.Len; i++ {
+			loc := followPath(overallMap, path, seed.Start+i)
+			if loc < minLoc {
+				minLoc = loc
+			}
+		}
+	}
+
+	return strconv.Itoa(minLoc)
+}
+
+func followPath(m map[string]*ValMap, path []string, input int) int {
+	val := input
+
+	for _, p := range path {
+		val = m[p].Calc(val)
+	}
+
+	return val
+}
+
+func findPath(list []string, required string) []string {
+	order := []string{}
+
+	for _, path := range list {
+		if path == required {
+			order = append(order, path)
+			return order
+		}
+	}
+
+	comps := strings.Split(required, "-")
+	src := comps[0]
+	dst := comps[1]
+
+	for _, path := range list {
+		if path == required {
+			order = append(order, path)
+			return order
+		}
+
+		subComps := strings.Split(path, "-")
+
+		subSrc := subComps[0]
+		subDst := subComps[1]
+
+		if subDst == dst {
+			newKey := fmt.Sprintf("%s-%s", src, subSrc)
+			order = append(findPath(list, newKey), path)
+			return order
+		}
+	}
+
+	panic("could not find a path")
+}
+
+func produceRange(destRange, sourceRange, rangeLen int, m *ValMap) *ValMap {
+	if m == nil {
+		m = &ValMap{}
+	}
+
+	rule := ValStruct{destRange, sourceRange, rangeLen}
+
+	m.Rules = append(m.Rules, rule)
+
+	return m
+}
