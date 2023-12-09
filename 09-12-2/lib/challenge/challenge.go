20fe9eacb875f5ab053cd49acdff15915ba2f5bb
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2023-12-09 22:52:59 +0000

    day 9

---
+++ 09-12-2/lib/challenge/challenge.go
@@ -0,0 +1,40 @@
+package challenge
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/lspaccatrosi16/go-cli-tools/algorithm"
+)
+
+func Challenge(input string) string {
+	lines := strings.Split(input, "\n")
+
+	tally := 0
+
+	for _, l := range lines {
+		terms := strings.Split(l, " ")
+
+		nums := []int{}
+
+		for _, t := range terms {
+			if t == "" {
+				continue
+			}
+
+			vInt, err := strconv.Atoi(t)
+			if err != nil {
+				panic("int conv error")
+			}
+
+			nums = append(nums, vInt)
+		}
+		seq := algorithm.SolveSequence(nums...)
+
+		newVal := seq.Get(0)
+
+		tally += newVal
+	}
+
+	return strconv.Itoa(tally)
+}
